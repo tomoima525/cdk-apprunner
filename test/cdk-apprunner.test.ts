a553a1db62edadfd619234b31418cb55d848dfb9
Tomoaki Imai <tomoima525@gmail.com>
2022-07-12 22:22:41 -0700

    Initial commit

---
+++ test/cdk-apprunner.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkApprunner from '../lib/cdk-apprunner-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-apprunner-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkApprunner.CdkApprunnerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
